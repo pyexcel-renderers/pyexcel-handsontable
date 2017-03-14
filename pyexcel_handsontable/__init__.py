9a662ae472f0b02e63a697d226cc175037b5e6ac
chfw <wangc_2011@hotmail.com>
2017-03-14 23:09:10 +0000

    update plugin protocol

--- pyexcel_handsontable/__init__.py
+++ pyexcel_handsontable/__init__.py
@@ -1,1 +1,8 @@
-__pyexcel_renderer_plugins__ = ['handsontable']
+__META__ = {
+    'plugin_type': 'renderer',
+    'submodule': 'handsontable',
+    'file_types': ['handsontable.html'],
+    'stream_type': 'string'
+}
+
+__pyexcel_plugins__ = [__META__]
