df1eb40f2ddbb6b3fd288633174cc7625015e563
chfw <wangc_2011@hotmail.com>
2017-04-28 23:10:29 +0100

    bug fix on the import loop, it is so big that its attribute were not registered onto Sheet and Book attributes. one solutin is here to remove the dependency on pyexcel's own plugin. other solution would be to do event trigger. when an new attribute becomes aware, register it on demand

--- pyexcel_handsontable/__init__.py
+++ pyexcel_handsontable/__init__.py
@@ -7,11 +7,22 @@
     :copyright: (c) 2016-2017 by Onni Software Ltd.
     :license: New BSD License, see LICENSE for further details
 """
-from pyexcel.internal.common import PyexcelPluginList
+from lml.registry import PluginList, PluginInfo
 
 
-PyexcelPluginList(__name__).add_a_renderer(
-    submodule='handsontable.HandsonTable',
-    file_types=['handsontable.html'],
-    stream_type='string'
+class MyPluginInfo(PluginInfo):
+
+    def keywords(self):
+        file_types = self.file_types
+        for file_type in file_types:
+            yield file_type
+
+
+PluginList(__name__)._add_a_plugin(
+    MyPluginInfo(
+        'renderer',
+        '%s.handsontable.HandsonTable' % __name__,
+        file_types=['handsontable.html'],
+        stream_type='string'
+    )
 )
